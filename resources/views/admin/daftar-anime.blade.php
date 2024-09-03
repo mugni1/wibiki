f342283a67fda5817ba40bdee9797e686f25dc07
malascoding <asepabdulmugni22@gmail.com>
2024-09-03 12:48:47 +0700

    update search on daftar-anime

--- resources/views/admin/daftar-anime.blade.php
+++ resources/views/admin/daftar-anime.blade.php
@@ -22,8 +22,8 @@
     {{-- SECTION SEARCH ANIME --}}
     <section class="px-5 w-full mx-auto pb-5">
         <form class=" w-full flex flex-wrap" action="" method="GET">
-            <input class=" w-10/12 border py-2 px-3 rounded-s-md md:w-11/12" type="text" name="keywords" placeholder="Search of name ..." id="">
-            <button class=" w-2/12 py-1 px-3 bg-slate-600 rounded-e-md text-white md:w-1/12 flex justify-center items-center " type="submit">
+            <input class=" w-10/12 border py-2 px-3 rounded-s-md md:w-11/12 outline-none" type="text" name="keywords" placeholder="Search of name ..." id="">
+            <button class=" w-2/12 py-1 px-3 bg-slate-600 rounded-e-md text-white md:w-1/12 flex justify-center items-center" type="submit">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class=" fill-current w-8 h-8">
                     <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
                     <path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path>
